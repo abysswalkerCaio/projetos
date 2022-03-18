aeefb84d3af6e86c4e75ffb467d4c9af1aa8236b
Caio Costa <hcaio370@gmail.com>
2022-03-18 16:36:07 -0300

    Update and rename mostrarNumeroAleatorio.java to MostrarNumeroAleatorio.java

--- MostrarNumeroAleatorio.java
+++ MostrarNumeroAleatorio.java
@@ -1,10 +1,10 @@
 package projetos;
 
-public class mostrarNumeroAleatorio {
+public class MostrarNumeroAleatorio {
 	public static void main(String[] args) {
-		gerarNumeroAleatorio gerar = new gerarNumeroAleatorio();
+		GerarNumeroAleatorio gerar = new GerarNumeroAleatorio();
 		System.out.println("Serão gerados 5 cadeias de números aleatórios de 0 até 50!");
-	    gerar.createRandomNumber(0, 1);
+	        gerar.createRandomNumber(0, 1);
 		gerar.createRandomNumber(0, 2);
 		gerar.createRandomNumber(0, 3);
 		gerar.createRandomNumber(0, 4);
