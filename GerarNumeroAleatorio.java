137ada1e1e37133eca10b801cb8994f3785515d3
Caio Costa <hcaio370@gmail.com>
2022-03-18 16:36:43 -0300

    Update GerarNumeroAleatorio.java

--- GerarNumeroAleatorio.java
+++ GerarNumeroAleatorio.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class GerarNumeroAleatorio {
 	public void createRandomNumber(int qualquernumero, int demonstracao) {
 		Random gerar = new Random();
-		qualquernumero = gerar.nextInt(1, 51); //Começa de 1, vai até 50.
-		/* Outra forma de se utilizar a classe Random:
-		 * gerar.nextInt(50) + 1;
+		qualquernumero = gerar.nextInt(1, 51); // Começa de 1, vai até 50.
+		/*
+		 * Outra forma de se utilizar a classe Random: gerar.nextInt(50) + 1;
 		 */
 		System.out.println(demonstracao + " - Um número aleatório de 1 até 50: " + qualquernumero);
 	}
