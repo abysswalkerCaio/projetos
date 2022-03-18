6de6ca670f32e64ff5430ceaa7fb6fafbe91c2d8
Caio Costa <hcaio370@gmail.com>
2022-03-18 15:10:13 -0300

    Update AlterarNome.java

--- AlterarNome.java
+++ AlterarNome.java
@@ -5,13 +5,12 @@ import java.util.Scanner;
 public class AlterarNome {
 	public static void main(String[] args) {
 		Scanner nomes = new Scanner(System.in);
-		String primeiro, ultimo, primeiroalterado;
+		String primeiro, ultimo;
 		System.out.println("Digite o primeiro nome: ");
 		primeiro = nomes.nextLine();
 		System.out.println("Informe o último nome: ");
 		ultimo = nomes.nextLine();
-		primeiroalterado = primeiro.substring(0, 1);
 		nomes.close();
-		System.out.println(primeiroalterado + ". " + ultimo);
+		System.out.println(primeiroalterado.charAt(0) + ". " + ultimo);
 	}
 }
