8b762cd550bd43d2f90c4162e96d2ce46400d56f
Caio Costa <hcaio370@gmail.com>
2022-03-22 17:26:19 -0300

    Create UsarConstrutorEThis.java

---
+++ UsarConstrutorEThis.java
@@ -0,0 +1,15 @@
+package projetos;
+
+public class UsarConstrutorEThis {
+	String nome, genero;
+	int idade;
+	double altura;
+
+	// Criando um construtor:
+	public UsarConstrutorEThis(String nome, String genero, int idade, double altura) {
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.altura = altura;
+	}
+}
