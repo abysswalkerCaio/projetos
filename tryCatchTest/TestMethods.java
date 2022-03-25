86b11e57aba81aeec3991322ca038c688b861dab
Caio Costa <hcaio370@gmail.com>
2022-03-25 15:36:19 -0300

    Create TestMethods.java

---
+++ tryCatchTest/TestMethods.java
@@ -0,0 +1,13 @@
+package tryCatchTest;
+
+public class TestMethods {
+	public int divisao(int x, int y) {
+		return x / y;
+	}
+	
+	public int matriz(int numeroMatriz) {
+		int [] numeros = {1, 2 ,3};
+		numeroMatriz = numeros[3];
+		return numeroMatriz;
+	}
+}
