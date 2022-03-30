6bfbebece92d17bb2ed37f82027cb0ae2b301d07
Caio Costa <hcaio370@gmail.com>
2022-03-30 20:50:11 -0300

    Create PanelVisualEditor.java

---
+++ dadosPessoa/PanelVisualEditor.java
@@ -0,0 +1,17 @@
+package dadosPessoa;
+
+import java.awt.Font;
+import javax.swing.JDialog;
+import javax.swing.UIManager;
+import javax.swing.plaf.ColorUIResource;
+
+public class PanelVisualEditor {
+
+	static public void mudarPainel() {
+		UIManager.put("OptionPane.background", new ColorUIResource(220, 220, 220));
+		UIManager.put("Panel.background", new ColorUIResource(220, 220, 220));
+		UIManager.put("OptionPane.okButtonText", "Continuar");
+		UIManager.put("OptionPane.messageFont", new Font("Arial", Font.BOLD, 13));
+		JDialog.setDefaultLookAndFeelDecorated(true);
+	}
+}
